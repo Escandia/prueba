c8cb9c441d4db77d9caabb9d22f4531d011ccbb5
Escandia Camargo <camargoescandia@yahoo.com>
2018-07-30 04:50:09 +0200

    Add files via upload
    prueba

---
+++ src/app/modules/restaurant/restaurant.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RestaurantComponent } from './restaurant.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [RestaurantComponent]
+})
+export class RestaurantModule { }
